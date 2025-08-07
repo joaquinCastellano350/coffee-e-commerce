52c298de18cd7acd662b81b2db648abcbd09c30b
joaquinCastellano350 <joaquincastellano350@gmail.com>
2025-08-06 23:11:08 -0300

    basic Catalog CRUD

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import {Request, Response, ErrorRequestHandler} from 'express'
 import {productRouter} from './product/product.routes.js'
 import { categoryRouter } from './category/category.routes.js';
+import { catalogRouter } from './catalog/catalog.routes.js';
 import { errorHandler } from './middlewares/error.middleware.js';
 
 import { connectDB } from './config/mongoose.js';
@@ -11,6 +12,7 @@ connectDB();
 app.use(express.json());
 app.use('/products', productRouter);
 app.use('/categories', categoryRouter);
+app.use('/catalogs', catalogRouter);
 app.use(errorHandler as ErrorRequestHandler);
 
 app.listen(3000, () => {
---
+++ src/catalog/catalog.routes.ts
@@ -0,0 +1,15 @@
+import { validate } from "../middlewares/validation.middleware.js";
+import { CatalogController } from "./catalog.controller.js";
+import { Router } from "express";
+import { createCatalogSchema, updateCatalogSchema } from "./catalog.validation.js";
+
+const catalogController = new CatalogController();
+export const catalogRouter = Router();
+
+catalogRouter.post("/", validate(createCatalogSchema), catalogController.createCatalog);
+catalogRouter.get("/", catalogController.getAllCatalogs);
+catalogRouter.get("/:id", catalogController.getCatalogById);
+catalogRouter.put("/:id", validate(updateCatalogSchema), catalogController.updateCatalog);
+catalogRouter.delete("/:id", catalogController.deleteCatalog);
+catalogRouter.patch("/:id/disable", catalogController.disableCatalog);
+catalogRouter.patch("/:id/enable", catalogController.enableCatalog);
